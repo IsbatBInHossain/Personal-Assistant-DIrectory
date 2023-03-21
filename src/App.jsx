64b9494a6c60678e1aa70d0dca8e9a72aa138e36
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-03-21 23:09:09 +0600

    Add CSS using bulma library and Finish the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
+import "bulma/css/bulma.css"
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png"
 import CortanaImage from "./images/cortana.png"
 import SiriImage from "./images/siri.png"
 
 const App = (props) => (
-    <div>
-      <div>Personal Assistance Directory</div>
-      <ProfileCard title="Alexa" handle="@alexa44" imgSrc={AlexaImage}/>
-      <ProfileCard title="Cortana" handle="@cortana11" imgSrc={CortanaImage}/>
-      <ProfileCard title="Siri" handle="@siri31" imgSrc={SiriImage}/>
-    </div>
+          <div>
+            <section className="hero is-primary">
+              <div className="hero-body">
+                <p className="title">
+                    Personal Assistant Directory
+                </p>
+    {/* <p class="subtitle">
+      Primary subtitle
+    </p> */}
+                </div>
+              </section>
+            <div className="container">
+              <section className="section">
+                <div className="columns">
+                  <div className="column is-4">
+                  <ProfileCard 
+                    title="Alexa" 
+                    handle="@alexa44" 
+                    imgSrc={AlexaImage}
+                    content="Amazon Alexa, also known simply as Alexa, is a virtual assistant technology largely based on a Polish speech synthesiser named Ivona, bought by Amazon in 2013."
+                  />
+                  </div>
+                  <div className="column is-4">
+                    <ProfileCard 
+                      title="Cortana" 
+                      handle="@cortana11" 
+                      imgSrc={CortanaImage}
+                      content="Cortana is a virtual assistant developed by Microsoft that uses the Bing search engine to perform tasks such as setting reminders and answering questions for the user."
+                    />
+                  </div>
+                  <div className="column is-4">
+                      <ProfileCard 
+                        title="Siri" 
+                        handle="@siri31" 
+                        imgSrc={SiriImage}
+                        content="Siri is a virtual assistant that is part of Apple Inc.'s iOS, iPadOS, watchOS, macOS, tvOS, and audioOS operating systems."  
+                      />
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
   );
 
   export default App;
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,9 +1,18 @@
-const ProfileCard = ({title, handle, imgSrc}) =>{
+const ProfileCard = ({title, handle, imgSrc,content}) =>{
     return (
-        <div>
-            <img src={imgSrc} alt="pda logo"/>
-            <div>The title is {title}</div>
-            <div>The handlename is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={imgSrc} alt="pda logo"/>
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">{content}</div>
+            </div>
         </div>
     )
 };
